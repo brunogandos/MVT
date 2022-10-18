a2bdf2c9078ee58c7b3bd012f26bf775ed7c4c38
brunogandos <brunogandos17@gmail.com>
2022-10-17 22:46:09 -0300

    clase19

---
+++ seed_data.py
@@ -0,0 +1,10 @@
+from members.models import Members
+
+
+Members(firstname = 'Bruno', lastname = 'Gandos', fecha = "2000-02-29").save()
+Members(firstname = 'Monica', lastname = 'Telis', fecha = "1978-12-2").save()
+Members(firstname = 'Manuela', lastname = 'Gandos', fecha = "2005-4-14").save()
+
+
+
+print("Todo okay")
