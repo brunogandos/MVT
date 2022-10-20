4648842cbef7b39fd677a1c63921d283e7d5f68d
brunogandos <brunogandos17@gmail.com>
2022-10-19 22:49:43 -0300

    agregadas las fechas

--- members/models.py
+++ members/models.py
@@ -5,3 +5,7 @@ class Members(models.Model):
   firstname = models.CharField(max_length=255)
   lastname = models.CharField(max_length=255)
   fecha = models.DateField(default=datetime.today)
+  
+  def __str__(self):
+    return f"{self.firstname},{self.lastname},{self.fecha}"
+
